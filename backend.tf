4130d0faf99998db87b5e81a37005dc050e53993
bhagyashrishinde1996 <bhagyashritupe96@gmail.com>
2023-10-25 21:51:47 +0530

    Updated bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-tf-state-lock-demo"
+    bucket         = "ctl-tf-state-lock-demo-1"
     key            = "hrms-demo"
     region         = "ap-south-1"
     dynamodb_table = "tf-lock-state"
